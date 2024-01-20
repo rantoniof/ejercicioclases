d51a0787d33cc2339a94c5b4c76f814145875c7b
Tony FLORES VALLADARES <r.flores@unitec.edu>
2024-01-19 19:28:59 -0600

    #ejercicio1#

---
+++ ejercicioclases/Ave.cs
@@ -0,0 +1,41 @@
+﻿using ejercicioclases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioclases
+{
+    internal class Ave
+    {
+        public string? Nombre;
+
+        public string? Color;
+
+        public string? Tamano;
+
+        public string? Familia;
+
+        public string? SexoBiologico;
+
+        public bool? EsVoladora;
+
+        public Ave() 
+        {
+            EsVoladora = true;   
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine($"Nombre:   {Nombre} ");
+            Console.WriteLine($"Tamaño:  {Tamano}");
+            Console.WriteLine($"Familia:  {Familia}");
+            Console.WriteLine($"Color:  {Color}");
+            Console.WriteLine($"Sexo biologico: {SexoBiologico}");
+
+        }
+    }
+
+  
+}
--- ejercicioclases/Program.cs
+++ ejercicioclases/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ejercicioclases;
+
+Ave ave = new Ave();
+ave.Nombre = "Avestruz";
+ave.Tamano = "Grande";
+ave.Color = "Café";
+ave.Familia = "aveztrufus";
+ave.SexoBiologico = "Macho";
+ave.imprimir();
+
+Ave ave2 = new Ave();
+ave2.Nombre = "Gallina";
+ave2.Tamano = "Pequeña";
+ave2.Color = "Café";
+ave2.Familia = "aveztrufus";
+ave2.SexoBiologico = "Macho";
+ave2.imprimir();
+
+
+// See https://aka.ms/new-console-template for more information
